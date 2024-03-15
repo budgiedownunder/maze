38980e19cacb200598e8001bd2de33ef52bb3e68
Chris Budgen <cbudgen@cbintex.com>
2024-03-15 14:08:46 +1100

    Modify it_works()

--- src/lib/solvers/solver-rust/src/lib.rs
+++ src/lib/solvers/solver-rust/src/lib.rs
@@ -8,7 +8,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(14, 14);
-        assert_eq!(result, 28);
+        let result = add(15, 15);
+        assert_eq!(result, 30);
     }
 }
