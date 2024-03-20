3c79fa47ee097a03d4763a5e3ff1aeb61aec93c3
Chris Budgen <cbudgen@cbintex.com>
2024-03-20 11:47:33 +1100

    Add support for start and end locations

--- src/rust/maze/src/maze.rs
+++ src/rust/maze/src/maze.rs
@@ -13,11 +13,19 @@ impl Maze {
         Maze { definition }
     }
     pub fn from_vec(grid: Vec<Vec<i32>>) -> Self {
-        Maze { definition: Definition::from_vec(grid) }
+        Maze {
+            definition: Definition::from_vec(grid),
+        }
     }
-    pub fn solve(&self) -> Result<Solution, SolveError> {
+    pub fn solve(
+        &self,
+        start_row: usize,
+        start_col: usize,
+        end_row: usize,
+        end_col: usize,
+    ) -> Result<Solution, SolveError> {
         let s = Solver { maze: &self };
-        s.solve()
+        s.solve(start_row, start_col, end_row, end_col)
     }
 }
 
@@ -27,22 +35,18 @@ mod tests {
 
     #[test]
     fn can_create_new_maze_from_vector() {
-        let grid: Vec<Vec<i32>> = vec![
-            vec![1, 2, 3],
-            vec![4, 5, 6],
-        ];
+        let grid: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6]];
         let m = Maze::from_vec(grid);
         assert_eq!(m.definition.rows, 2);
         assert_eq!(m.definition.cols, 3);
     }
 
     #[test]
-    #[should_panic(expected = "Grid vector contains rows with different numbers of columns (expected 3 for all rows)")]
+    #[should_panic(
+        expected = "Grid vector contains rows with different numbers of columns (expected 3 for all rows)"
+    )]
     fn cannot_create_new_maze_definition_from_vector_with_diff_row_counts() {
-        let grid: Vec<Vec<i32>> = vec![
-            vec![1, 2, 3],
-            vec![4, 5, 6, 7],
-        ];
+        let grid: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6, 7]];
         let _d = Definition::from_vec(grid);
     }
 
@@ -54,16 +58,42 @@ mod tests {
     }
 
     #[test]
-    fn maze_solve_should_fail_with_not_implemented() {
+    fn maze_solve_should_fail_with_invalid_start_location() {
         let m = Maze::new(Definition::new(2, 3));
-        let result = m.solve();
+        let result = m.solve(2, 0, 0, 2);
         match result {
             Ok(_) => {
-                // If solve() returns Ok, fail the test
                 panic!("Expected solve() to return Err, but it returned Ok");
             }
             Err(e) => {
-                // Assert if "Not implemented" is not returned
+                assert_eq!(e.message, "Start location [2, 0] is invalid");
+            }
+        }
+    }
+
+    #[test]
+    fn maze_solve_should_fail_with_invalid_end_location() {
+        let m = Maze::new(Definition::new(2, 3));
+        let result = m.solve(1, 0, 0, 3);
+        match result {
+            Ok(_) => {
+                panic!("Expected solve() to return Err, but it returned Ok");
+            }
+            Err(e) => {
+                assert_eq!(e.message, "End location [0, 3] is invalid");
+            }
+        }
+    }
+
+    #[test]
+    fn maze_solve_should_fail_with_not_implemented() {
+        let m = Maze::new(Definition::new(2, 3));
+        let result = m.solve(1, 0, 0, 2);
+        match result {
+            Ok(_) => {
+                panic!("Expected solve() to return Err, but it returned Ok");
+            }
+            Err(e) => {
                 assert_eq!(e.message, "Not implemented");
             }
         }
--- src/rust/maze/src/solver.rs
+++ src/rust/maze/src/solver.rs
@@ -1,5 +1,5 @@
-use crate::Maze;
 use crate::solution::Solution;
+use crate::Maze;
 
 use std::error::Error;
 
@@ -25,14 +25,37 @@ impl std::fmt::Display for SolveError {
 impl Error for SolveError {}
 
 #[allow(dead_code)]
-pub struct Solver <'a>{
+pub struct Solver<'a> {
     pub maze: &'a Maze,
 }
 
-impl Solver <'_> {
-    pub fn solve(&self) -> Result<Solution, SolveError> {
+impl Solver<'_> {
+    fn is_valid_location(&self, row_idx: usize, col_idx: usize) -> bool {
+        if row_idx >= self.maze.definition.rows || col_idx >= self.maze.definition.cols {
+            return false;
+        }
+        true
+    }
+
+    pub fn solve(
+        &self,
+        start_row: usize,
+        start_col: usize,
+        end_row: usize,
+        end_col: usize,
+    ) -> Result<Solution, SolveError> {
+        if !self.is_valid_location(start_row, start_col) {
+            return Err(SolveError::new(
+                format!("Start location [{}, {}] is invalid", start_row, start_col).as_str(),
+            ));
+        }
+        if !self.is_valid_location(end_row, end_col) {
+            return Err(SolveError::new(
+                format!("End location [{}, {}] is invalid", end_row, end_col,).as_str(),
+            ));
+        }
+
         Err(SolveError::new("Not implemented"))
         //Ok(())
     }
 }
-
