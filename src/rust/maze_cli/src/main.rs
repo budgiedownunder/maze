1ced58f8e681d47c3333b8f81440e11e3ad0ab8d
Chris Budgen <cbudgen@cbintex.com>
2024-04-24 15:46:52 +1000

    Add dummy menu to CLI and supporting test to confirm stdout has expected content

--- src/rust/maze_cli/src/main.rs
+++ src/rust/maze_cli/src/main.rs
@@ -1,28 +1,77 @@
 use maze::Definition;
 use maze::Maze;
 
-static WELCOME_BANNER: &'static str = r#"
-    ******************************
-    * Welcome to the Maze CLI !! *
-    ******************************
+fn str_lines(s: &'static str) -> Vec<&'static str> {
+    s.lines().map(|line| line.trim_start()).collect()
+}
+
+fn print_lines(lines: Vec<&'static str>) {
+    for line in lines {
+        println!("{}", line);
+    }
+}
+
+static WELCOME_BANNER: &'static str = r#"******************************
+       * Welcome to the Maze CLI !! *
+       ******************************
     "#;
 
+fn welcome_banner_lines() -> Vec<&'static str> {
+    str_lines(WELCOME_BANNER)
+}
+
 fn print_welcome_banner() {
-    for line in WELCOME_BANNER.lines() {
-        println!("{}", line.trim_start());
-    }
+    print_lines(welcome_banner_lines());
+}
+
+static MENU: &'static str = r#"******************************
+        Select action:
+
+        P -> Print maze
+        Q -> Quit
+        ******************************
+        "#;
+
+fn menu_lines() -> Vec<&'static str> {
+    str_lines(MENU)
+}
+
+fn print_menu() {
+    print_lines(menu_lines());
 }
 
 fn main() {
     print_welcome_banner();
+    print_menu();
     let _m = Maze::new(Definition::new(0, 0));
 }
 
-
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use assert_cmd::Command;
+
+    // use predicates::str::contains;
+
     #[test]
-    fn dummy_test() {
-        assert_eq!(1, 1);
+    fn should_display_welcome_and_menu_on_start() {
+        let output = Command::cargo_bin(app_name()).unwrap().output().unwrap();
+        let mut expected_lines = welcome_banner_lines();
+        expected_lines.extend(menu_lines());
+
+        let std_output = String::from_utf8_lossy(&output.stdout);
+        assert_lines_eq(std_output.lines().collect(), expected_lines);
     }
-}    
+
+    // Helper functions
+    fn app_name() -> &'static str {
+        env!("CARGO_PKG_NAME")
+    }
+
+    fn assert_lines_eq(actual_lines: Vec<&str>, expected_lines: Vec<&str>) {
+        assert_eq!(actual_lines.len(), expected_lines.len());
+        for (actual_line, expected_line) in actual_lines.iter().zip(expected_lines.iter()) {
+            assert_eq!(*actual_line, *expected_line);
+        }
+    }
+}
