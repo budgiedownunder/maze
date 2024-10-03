7a75b57065fe05b1c987ca80c230dddce019d802
Chris Budgen <cbudgen@cbintex.com>
2024-10-03 14:36:59 +1000

    Add comments to lib.rs

--- src/rust/maze_wasm/src/lib.rs
+++ src/rust/maze_wasm/src/lib.rs
@@ -1,6 +1,9 @@
 pub mod wasm_common;
+
+// Export 'wasm' module if not in wasm-bindgen mode
 #[cfg(not(feature = "wasm-bindgen"))]
 pub mod wasm;
 
+// Export 'wasm_bindgen' module if in wasm-bindgen mode
 #[cfg(feature = "wasm-bindgen")]
 pub mod wasm_bindgen;
