775b5c026bc3f56c3023a2528c82582849960890
Chris Budgen <cbudgen@cbintex.com>
2024-11-16 11:54:56 +1100

    Add documentation for Pointer class and Icon enum in Maze.Maui.App

--- src/csharp/Maze.Maui.App/Controls/Pointer/Icon.cs
+++ src/csharp/Maze.Maui.App/Controls/Pointer/Icon.cs
@@ -1,14 +1,38 @@
 ﻿
 namespace Maze.Maui.App.Controls.Pointer
 {
+    /// <summary>
+    /// Represents a type of icon for use as a pointer
+    /// </summary>
     public enum Icon
     {
+        /// <summary>
+        /// Wait
+        /// </summary>
         Wait,
+        /// <summary>
+        /// Arrow
+        /// </summary>
         Arrow,
+        /// <summary>
+        /// Size all
+        /// </summary>
         SizeAll,
+        /// <summary>
+        /// Size either north-east or south-west
+        /// </summary>
         SizeNorthEastSouthWest,
+        /// <summary>
+        /// Size either north or south
+        /// </summary>
         SizeNorthSouth,
+        /// <summary>
+        /// Size either north-west or south-east
+        /// </summary>
         SizeNorthWestSouthEast,
-        SizeWestEast,
+        /// <summary>
+        /// Size either west or east
+        /// </summary>
+        SizeWestEast
     }
 }
