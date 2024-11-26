93443609cca613fb52345586fe5cb4607d69b5b7
Chris Budgen <cbudgen@cbintex.com>
2024-11-26 16:50:33 +1100

    Add (1) dummy endpoint for GET /api/v1/mazes and Swagger/OpenAPI endpoint api-docs/v1/swagger-ui/ + api-docs/v1/openapi.json (2) logging

--- src/rust/maze_web_server/src/api/mod.rs
+++ src/rust/maze_web_server/src/api/mod.rs
@@ -1,3 +1,26 @@
 pub mod v1;
-pub mod shared; 
 
+use actix_web::web;
+use lazy_static::lazy_static;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+// Lazy static so Swagger json is only initialized once (as opposed to per worker)
+lazy_static! {
+    static ref SWAGGER_UI: SwaggerUi = {
+        let openapi_v1 = v1::openapi::ApiDocV1::openapi();
+        SwaggerUi::new("api-docs/v1/swagger-ui/{_:.*}").url("/api-docs/v1/openapi.json", openapi_v1)
+    };
+}
+
+pub fn register_api() -> actix_web::Scope {
+    web::scope("api")
+        .service(
+            web::scope("v1")
+                .configure(v1::routes::configure)
+            )
+}
+
+pub fn register_swagger_ui() -> SwaggerUi {
+    SWAGGER_UI.clone()
+}
--- src/rust/maze_web_server/src/main.rs
+++ src/rust/maze_web_server/src/main.rs
@@ -1,6 +1,22 @@
-// mod api;
-// use crate::api::v1;
+mod api;
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ App, middleware::Logger, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // TO DO - make these  environment/config settings with defaults
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let address = "127.0.0.1:8080";
+    let max_workers = std::thread::available_parallelism()?; // This is actix_web's default too
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+           .service(api::register_api())
+           .service(api::register_swagger_ui())
+    })
+    .bind(address)?
+    .workers(usize::from(max_workers))
+    .run()
+    .await
 }
