4b994ab42c50ca3e68a7158526633161bc20c740
Chris Budgen <cbudgen@cbintex.com>
2024-12-27 16:27:24 +1100

    Disable cleanup_tocs .ps1script in build_all.bat

--- src/docfx/build_all.bat
+++ src/docfx/build_all.bat
@@ -7,9 +7,9 @@ rmdir /s /q web-doc-tmp
 call copy_files.bat
 docfx metadata docfx.json
 
-type api\net\toc.yml
+@REM type api\net\toc.yml
 
-powershell -ExecutionPolicy Bypass -File cleanup_tocs.ps1
+@REM powershell -ExecutionPolicy Bypass -File cleanup_tocs.ps1
 
 docfx build docfx.json
 
