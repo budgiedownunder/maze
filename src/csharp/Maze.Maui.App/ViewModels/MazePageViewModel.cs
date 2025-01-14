d10d41563b7032f13286e2babf7c9df65bf892ab
Chris Budgen <cbudgen@cbintex.com>
2025-01-14 13:08:30 +1100

    Refactor maze view model to use Community Toolkit for commands and obsersvable properties

--- src/csharp/Maze.Maui.App/ViewModels/MazePageViewModel.cs
+++ src/csharp/Maze.Maui.App/ViewModels/MazePageViewModel.cs
@@ -1,7 +1,6 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Maze.Maui.App.Models;
 using Maze.Maui.App.Services;
 using Maze.Maui.Services;
@@ -12,21 +11,6 @@ namespace Maze.Maui.App.ViewModels
     public partial class MazePageViewModel : BaseViewModel
     {
         private const int COMMAND_DELAY_MS = 50;
-
-        public ICommand InsertRowsCommand { get; }
-        public ICommand DeleteRowsCommand { get; }
-        public ICommand InsertColumnsCommand { get; }
-        public ICommand DeleteColumnsCommand { get; }
-        public ICommand SelectRangeCommand { get; }
-        public ICommand DoneCommand { get; }
-        public ICommand SetWallCommand { get; }
-        public ICommand SetStartCommand { get; }
-        public ICommand SetFinishCommand { get; }
-        public ICommand ClearCommand { get; }
-        public ICommand SolveCommand { get; }
-        public ICommand ClearSolutionCommand { get; }
-        public ICommand SaveCommand { get; }
-        public ICommand RefreshCommand { get; }
 
         public event EventHandler? InsertRowsRequested;
         public event EventHandler? DeleteRowsRequested;
@@ -49,210 +33,57 @@ namespace Maze.Maui.App.ViewModels
 
         private readonly MazesViewModel _mazesViewModel;
 
-        private bool _canInsertRows = false;
-        private bool _canDeleteRows = false;
-        private bool _canInsertColumns = false;
-        private bool _canDeleteColumns = false;
-        private bool _canSelectRange = false;
-        private bool _canShowDone = false;
-        private bool _canSetWall = false;
-        private bool _canSetStart = false;
-        private bool _canSetFinish = false;
-        private bool _canClear = false;
-        private bool _canSolve = false;
-        private bool _canClearSolution = false;
-        private bool _canSave = true;
-        private bool _canRefresh = false;
+        public bool IsStored { get; set; }
+        public bool IsDirty { get; set; }
+
+        [ObservableProperty]
+        MazeItem mazeItem = new MazeItem();
+
+        [ObservableProperty]
+        bool canInsertRows = false;
+
+        [ObservableProperty]
+        bool canDeleteRows = false;
+
+        [ObservableProperty]
+        bool canInsertColumns = false;
+
+        [ObservableProperty]
+        bool canDeleteColumns = false;
+
+        [ObservableProperty]
+        bool canSelectRange = false;
+
+        [ObservableProperty]
+        bool canShowDone = false;
+
+        [ObservableProperty]
+        bool canSetWall = false;
+
+        [ObservableProperty]
+        bool canSetStart = false;
+
+        [ObservableProperty]
+        bool canSetFinish = false;
+
+        [ObservableProperty]
+        bool canClear = false;
+
+        [ObservableProperty]
+        bool canSolve = false;
+
+        [ObservableProperty]
+        bool canClearSolution = false;
+
+        [ObservableProperty]
+        bool canSave = true;
+
+        [ObservableProperty]
+        bool canRefresh = false;
 
         public bool IsTouchOnlyDevice
         {
             get => _deviceTypeService.IsTouchOnlyDevice();
-        }
-
-        public bool IsStored { get; set; }
-        public bool IsDirty { get; set; }
-
-        public bool CanInsertRows
-        {
-            get => _canInsertRows;
-            set
-            {
-                if (_canInsertRows != value)
-                {
-                    _canInsertRows = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanDeleteRows
-        {
-            get => _canDeleteRows;
-            set
-            {
-                if (_canDeleteRows != value)
-                {
-                    _canDeleteRows = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanInsertColumns
-        {
-            get => _canInsertColumns;
-            set
-            {
-                if (_canInsertColumns != value)
-                {
-                    _canInsertColumns = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanDeleteColumns
-        {
-            get => _canDeleteColumns;
-            set
-            {
-                if (_canDeleteColumns != value)
-                {
-                    _canDeleteColumns = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanSelectRange
-        {
-            get => _canSelectRange;
-            set
-            {
-                if (_canSelectRange != value)
-                {
-                    _canSelectRange = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-
-        public bool CanShowDone
-        {
-            get => _canShowDone;
-            set
-            {
-                if (_canShowDone!= value)
-                {
-                    _canShowDone = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanSetWall
-        {
-            get => _canSetWall;
-            set
-            {
-                if (_canSetWall != value)
-                {
-                    _canSetWall = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanSetStart
-        {
-            get => _canSetStart;
-            set
-            {
-                if (_canSetStart != value)
-                {
-                    _canSetStart = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanSetFinish
-        {
-            get => _canSetFinish;
-            set
-            {
-                if (_canSetFinish != value)
-                {
-                    _canSetFinish = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanClear
-        {
-            get => _canClear;
-            set
-            {
-                if (_canClear != value)
-                {
-                    _canClear = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanSolve
-        {
-            get => _canSolve;
-            set
-            {
-                if (_canSolve != value)
-                {
-                    _canSolve = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanClearSolution
-        {
-            get => _canClearSolution;
-            set
-            {
-                if (_canClearSolution != value)
-                {
-                    _canClearSolution = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanSave
-        {
-            get => _canSave;
-            set
-            {
-                if (_canSave != value)
-                {
-                    _canSave= value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool CanRefresh
-        {
-            get => _canRefresh;
-            set
-            {
-                if (_canRefresh != value)
-                {
-                    _canRefresh = value;
-                    OnPropertyChanged();
-                }
-            }
         }
 
         public MazePageViewModel(IDeviceTypeService deviceTypeService, IMazeService mazeService, MazesViewModel mazesViewModel, IDialogService dialogService)
@@ -261,98 +92,98 @@ namespace Maze.Maui.App.ViewModels
             this._mazeService = mazeService;
             this._mazesViewModel = mazesViewModel;
             this._dialogService = dialogService;
-
-            InsertRowsCommand = new Command(async () => await OnInsertRows());
-            DeleteRowsCommand = new Command(async () => await OnDeleteRows());
-            InsertColumnsCommand = new Command(async () => await OnInsertColumns());
-            DeleteColumnsCommand = new Command(async () => await OnDeleteColumns());
-            SelectRangeCommand = new Command(async () => await OnSelectRange());
-            DoneCommand = new Command(async () => await OnDone());
-            SetWallCommand = new Command(async () => await OnSetWall());
-            SetStartCommand = new Command(async () => await OnSetStart());
-            SetFinishCommand = new Command(async () => await OnSetFinish());
-            ClearCommand = new Command(async () => await OnClear());
-            SolveCommand = new Command(async () => await OnSolve());
-            ClearSolutionCommand = new Command(async () => await OnClearSolution());
-            SaveCommand = new Command(async () => await OnSave());
-            RefreshCommand = new Command(async () => await OnRefresh());
-            _mazesViewModel = mazesViewModel;
-            _dialogService = dialogService;
         }
-        [ObservableProperty]
-        MazeItem mazeItem = new MazeItem();
 
-        private async Task OnInsertRows()
+        [RelayCommandAttribute]
+        private async Task InsertRowsAsync()
         {
             await RunCommand(InsertRowsRequested);
             UpdateCanSaveRefresh(true);
         }
 
-        private async Task OnDeleteRows()
+        [RelayCommandAttribute]
+        private async Task DeleteRowsAsync()
         {
             await RunCommand(DeleteRowsRequested);
             UpdateCanSaveRefresh(true);
         }
 
-        private async Task OnInsertColumns()
+        [RelayCommandAttribute]
+        private async Task InsertColumnsAsync()
         {
             await RunCommand(InsertColumnsRequested);
             UpdateCanSaveRefresh(true);
         }
 
-        private async Task OnDeleteColumns()
+        [RelayCommandAttribute]
+        private async Task DeleteColumnsAsync()
         {
             await RunCommand(DeleteColumnsRequested);
             UpdateCanSaveRefresh(true);
         }
 
-        private async Task OnSelectRange()
+        [RelayCommandAttribute]
+        private async Task SelectRangeAsync()
         {
             await RunCommand(SelectRangeRequested);
         }
 
-        private async Task OnDone()
+        [RelayCommandAttribute]
+        private async Task DoneAsync()
         {
             await RunCommand(DoneRequested);
         }
 
-        private async Task OnSetWall()
+        [RelayCommandAttribute]
+        private async Task SetWallAsync()
         {
             await RunCommand(SetWallRequested);
             UpdateCanSaveRefresh(true);
         }
 
-        private async Task OnSetStart()
+        [RelayCommandAttribute]
+        private async Task SetStartAsync()
         {
             await RunCommand(SetStartRequested);
             UpdateCanSaveRefresh(true);
         }
 
-        private async Task OnSetFinish()
+        [RelayCommandAttribute]
+        private async Task SetFinishAsync()
         {
             await RunCommand(SetFinishRequested);
             UpdateCanSaveRefresh(true);
         }
 
-        private async Task OnClear()
+        [RelayCommandAttribute]
+        private async Task ClearAsync()
         {
             await RunCommand(ClearRequested);
             UpdateCanSaveRefresh(true);
         }
 
-        private async Task OnSolve()
+        [RelayCommandAttribute]
+        private async Task SolveAsync()
         {
             await RunCommand(SolveRequested);
         }
 
-        private async Task OnClearSolution()
+        [RelayCommandAttribute]
+        private async Task ClearSolutionAsync()
         {
             await RunCommand(ClearSolutionRequested);
         }
 
-        private async Task OnSave()
+        [RelayCommandAttribute]
+        private async Task SaveAsync()
         {
             await RunCommand(SaveRequested);
+        }
+
+        [RelayCommandAttribute]
+        private async Task RefreshAsync()
+        {
+            await RunCommand(RefreshRequested);
         }
 
         public async Task<bool> SaveMaze(Api.Maze definition)
@@ -374,10 +205,7 @@ namespace Maze.Maui.App.ViewModels
             }
             catch (Exception ex)
             {
-                await _dialogService.ShowAlert("Error", $"Unable to save maze: {ex.Message}", "OK");
-            }
-            finally
-            {
+                await _dialogService.ShowAlert("Error", $"Failed to save maze: {ex.Message}", "OK");
             }
             return saved;
         }
@@ -415,17 +243,16 @@ namespace Maze.Maui.App.ViewModels
             MazeItem.Definition = definition;
         }
 
-        private async Task OnRefresh()
-        {
-            await RunCommand(RefreshRequested);
-        }
-
         public async Task<bool> RefreshMaze()
         {
             bool refreshed = false;
             if (await _dialogService.ShowConfirmation("Refresh Maze", 
                 "Are you sure you want to refresh the maze?\n\nNote: any changes you have made will be lost", 
                 "Yes", "No")) {
+
+                if (CanClearSolution)
+                    await ClearSolutionAsync();
+
                 try
                 {
                     IsBusy = true;
