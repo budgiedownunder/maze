aa6a5ceea22ca17aecc2a640951f44112420845f
Chris Budgen <cbudgen@cbintex.com>
2026-07-15 17:19:51 +1000

    maze_web_server (React): give the Play-3D hub tiles distinct, multi-color My Games / Shared / Community art

--- src/react/maze_web_server/src/pages/Play3dHubPage.tsx
+++ src/react/maze_web_server/src/pages/Play3dHubPage.tsx
@@ -5,9 +5,9 @@ import { AppHeader } from '../components/AppHeader'
 // "coming soon" placeholder until D4.6 (My Games / Shared) and D4.8 (Community).
 const TILES = [
   { to: '/play-3d/featured', img: '/images/workshop/workshop-features.svg', title: 'Featured', desc: 'Games and collections featured for everyone' },
-  { to: '/play-3d/my-games', img: '/images/workshop/workshop-game.svg', title: 'My Games', desc: 'Your own 3D games and collections' },
-  { to: '/play-3d/shared', img: '/images/workshop/workshop-game-collections.svg', title: 'Shared with me', desc: 'Games and collections others have shared with you' },
-  { to: '/play-3d/community', img: '/images/workshop/workshop-games.svg', title: 'Community', desc: 'Public games and collections from everyone' },
+  { to: '/play-3d/my-games', img: '/images/workshop/workshop-my-games.svg', title: 'My Games', desc: 'Your own 3D games and collections' },
+  { to: '/play-3d/shared', img: '/images/workshop/workshop-shared.svg', title: 'Shared with me', desc: 'Games and collections others have shared with you' },
+  { to: '/play-3d/community', img: '/images/workshop/workshop-community.svg', title: 'Community', desc: 'Public games and collections from everyone' },
 ] as const
 
 // The 3D Games hub: a signpost to the four browse scopes, mirroring the Workshop
